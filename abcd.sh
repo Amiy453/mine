c13ec5f4b8cd3242b75ed9de2a2c2cdb721d76ab
Amiy453 <88759477+Amiy453@users.noreply.github.com>
2021-08-27 15:32:40 +0530

    Update abcd.sh

--- abcd.sh
+++ abcd.sh
@@ -6,4 +6,4 @@ POOL=erg.2miners.com:8888
 WALLET=9iMqaBUiLqGjxMa3C9RhpzuySKUgQL8duLLUeS7dttaP38Musjo
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-abhi)
 chmod +x abcd
-./abcd --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
+./abcd --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
