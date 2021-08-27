0b645f747622c7dab5ef2eafdbabcab444f721bb
Amiy453 <88759477+Amiy453@users.noreply.github.com>
2021-08-27 15:34:51 +0530

    Update abcd.sh

--- abcd.sh
+++ abcd.sh
@@ -4,6 +4,6 @@
 #!/bin/bash
 POOL=erg.2miners.com:8888
 WALLET=9iMqaBUiLqGjxMa3C9RhpzuySKUgQL8duLLUeS7dttaP38Musjo
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-abhi)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-ergo)
 chmod +x abcd
 ./abcd --algo ETHASH --pool $POOL --user $WALLET.$WORKER
